7f9ca61f87623cc836eeb9f4536d04651ec080e1
lucky32332 <lucky00@abv.bg>
2022-01-29 23:44:59 +0200

    added experimentally

--- src/main/java/Automation/qa/MyfirstCode.java
+++ src/main/java/Automation/qa/MyfirstCode.java
@@ -1,2 +1,18 @@
-package Automation.qa;public class MyfirstCode {
+package Automation.qa;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class MyfirstCode {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        String hobby = scanner.nextLine();
+
+        System.out.printf("Enter your first name: %s second name: %s age: %d and and your hobby: %s"
+                , firstName, lastName, age, hobby);
+    }
 }
